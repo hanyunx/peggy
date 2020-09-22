7b7333c3a2787bc876af724fec7e4abcee42a742
hanyunx <hanyunx2@gmail.com>
2020-09-22 16:21:18 -0700

    Add more for comparison

--- solidity/test/happy-path.ts
+++ solidity/test/happy-path.ts
@@ -324,6 +324,30 @@ describe("Gas tests", function() {
     );
   });
 
+  it("testVerifyECDSA in isolation", async function() {
+    const signers = await ethers.getSigners();
+    const peggyId = ethers.utils.formatBytes32String("foo");
+
+    // This is the power distribution on the Cosmos hub as of 7/14/2020
+    let powers = examplePowers();
+    let validators = signers.slice(0, powers.length);
+
+    const powerThreshold = 6666;
+
+    const {
+      peggy,
+      testERC20,
+      checkpoint: deployCheckpoint
+    } = await deployContracts(peggyId, validators, powers, powerThreshold);
+
+    let sigs = await signHash(
+      validators,
+      "0x7bc422a00c175cae98cf2f4c36f2f8b63ec51ab8c57fecda9bccf0987ae2d67d"
+    );
+
+    await peggy.testEmptyBLS();
+  });
+
   it("testVerifyBLS in isolation", async function() {
     const signers = await ethers.getSigners();
     const peggyId = ethers.utils.formatBytes32String("foo");
